250c0f01ee5a81a2ac60139247e3047965b9b946
ckue-opportunity <ckue.opportunity@gmail.com>
2023-07-20 16:58:34 +0200

    feedback.php Bilanz

--- feedback.php
+++ feedback.php
@@ -3,7 +3,6 @@
   include "php/evaluate-user-input.php";
   include 'php/header.php';
 ?>
-
   <div class="row">
     <div class="col-sm-8">
       <!-- CONTENT -->
@@ -18,7 +17,8 @@
           echo "<p class='final-feedback'>" . "Bitte kümmere dich mehr um deine Gesundheit!" . "</p>";
         }
         elseif ($totalPoints < 24) {
-          echo "<p class='final-feedback'>" . "Du scheinst ok zu sein, könntest aber noch mehr für deine Gesundheit tun." . "</p>";
+          echo "<p class='final-feedback'>" . "Du scheinst ok zu sein, könntest aber noch mehr für deine Gesundheit tun." 
+               . "</p>";
         }
         else {
           echo "<p class='final-feedback'>" . "Gratuliere, du bist sehr fit!" . "</p>";
@@ -26,13 +26,10 @@
 
         echo "<p></p>";
       ?>
-
       <button type="button" class="btn btn-primary"
               onclick="document.location='/index.php'">Repeat</button>
-
       <p class="spacer"></p>
       <!-- END OF CONTENT -->
     </div>
   </div>
-
 <?php include 'php/footer.php'; ?>
